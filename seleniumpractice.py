20be590e95d042d941076a571fbd353c72ddd6e2
Kev <kzhong0508@gmail.com>
2024-10-22 23:12:51 -0700

    organize scraped date into dictionary

--- seleniumpractice.py
+++ seleniumpractice.py
@@ -4,7 +4,25 @@ from selenium.webdriver.common.by import By
 import time
 
 
-car_names = []
+all_cars = {}
+# vehicle = {
+#     "Model": "",
+#     "Year": "",
+#     "Current Bid": "",
+#     "Description": "",
+#     "Location": ""
+
+# }
+
+def add_car(car_id, model, year, current_bid, description, location):
+    all_cars[car_id] = {
+        "Model": model,
+        "Year": year,
+        "Current Bid": current_bid,
+        "Description": description,
+        "Location": location
+    }
+
 
 
 def scrape_cars_n_bids():
@@ -18,21 +36,52 @@ def scrape_cars_n_bids():
     driver.get(carsnbids) # open a chromedriver window
 
     time.sleep(1)
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # scroll to the bottom of page
+    # time.sleep(10)
+    
+    # below all need .text when iterating to get text
+    # all should have same length
+    car_titles = driver.find_elements(By.XPATH,'//div[@class="auction-title"]//a') # get list of car names, split into year and model when iterating
+    current_bids = driver.find_elements(By.XPATH, '//li[@class="auction-item "]//span[@class="bid-value"]') # get list of current bids
+    descriptions = driver.find_elements(By.XPATH, '//li[@class="auction-item "]//p[@class="auction-subtitle"]') # get list of descriptions
+    locations = driver.find_elements(By.XPATH, '//li[@class="auction-item "]//p[@class="auction-loc"]') # get list of locations
+
+    print(len(car_titles))
+    print(len(current_bids))
+    print(len(descriptions))
+    print(len(locations))
+
+    time.sleep(5)
+
+    for i in range (len(car_titles)):
+        # try:
+        car_text = car_titles[i].text
+        car_id = i + 1
+        year = car_text.split()[0]
+        model = " ".join(car_text.split()[1:])
+        # current_bid = current_bids[i].text
+        current_bid = "null"
+        description = descriptions[i].text
+        location = locations[i].text
+        add_car(car_id, model, year, current_bid, description, location)
+        # except IndexError:
+        #     print("index error")
     
 
-    cars = driver.find_elements(By.XPATH,'//div[@class="auction-title"]//a') 
 
-
-    for car in cars:
-        car_names.append(car.text)
+    # for car in cars:
+    #     car_names.append(car.text)
     
     print("end cars and bids")
 
     driver.quit()
 
+scrape_cars_n_bids()
+print(all_cars)
+
 def scrape_bat():
     """Scrapes the models of all cars for auction on Bring a Trailer"""
-
+    ## currently not working, need to separate keys
     bat = "https://bringatrailer.com/auctions/"
 
     cService = Service(executable_path="chromedriver.exe")
@@ -84,8 +133,8 @@ def scrape_bat():
     driver.quit()
 
 
-scrape_cars_n_bids()
-print(car_names, len(car_names))
-scrape_bat()
-print(car_names, len(car_names))
+# scrape_cars_n_bids()
+# print(car_names, len(car_names))
+# scrape_bat()
+# print(car_names, len(car_names))
 
